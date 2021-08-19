7d8ee6d0dece32f76c1234da3ef816379089dc1c
kochx384 <kochx384@gmail.com>
2021-08-18 19:45:02 -0500

    add three new csv files for deliverable 1

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,30 @@
+-- Deliverable 1: Number of Retiring Employees By Title
+-- Create retirement_titles table
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	titles.title,
+	titles.from_date,
+	titles.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles
+ON (e.emp_no = titles.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Create counts by retiring titles table
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY unique_titles.title
+ORDER BY COUNT (title) DESC;
